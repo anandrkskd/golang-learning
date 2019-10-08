dcdedc565441116129191927a8409ee19c6b9adb
anandrkskd <anandrkskd@gmail.com>
2019-10-09 01:14:40 +0530

    new way to check os

---
+++ basic/checkWindows.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+
+	if runtime.GOOS == "windows" {
+		fmt.Println("You are running on Windows")
+	} else {
+		fmt.Println("You are running on an OS other than Windows")
+		fmt.Print(runtime.GOOS)
+	}
+
+}
